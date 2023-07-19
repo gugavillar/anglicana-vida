4c0de37c787e4c70ec42efe3d83ce866aff78801
Guga Villar <grsv.21@gmail.com>
2023-07-19 20:24:49 -0300

    :sparkles: Feat: add chakraui provider and create prismic configuration for preview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+
+import { repositoryName } from '@/prismicio'
+import { PrismicPreview } from '@prismicio/next'
+
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+      <PrismicPreview repositoryName={repositoryName} />
+    </ChakraProvider>
+  )
 }
