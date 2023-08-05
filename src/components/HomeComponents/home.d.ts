8c3b771b9bf3dd119133c99e1b4b78248cd2d624
Guga Villar <grsv.21@gmail.com>
2023-08-05 17:48:19 -0300

    :sparkles: Feat: Create Home page

---
+++ src/components/HomeComponents/home.d.ts
@@ -0,0 +1,9 @@
+import { RichTextField, TitleField } from '@prismicio/client'
+
+export type CellCardType = {
+  name: TitleField
+  location: RichTextField
+  leader: RichTextField
+  week_day: RichTextField
+  initial_time: RichTextField
+}
---
+++ src/helpers/getHome.ts
@@ -0,0 +1,30 @@
+import { GetStaticPropsContext } from 'next'
+
+import { createClient } from '@/prismicio'
+
+export const getHome = async ({ previewData }: GetStaticPropsContext) => {
+  const client = createClient({ previewData })
+
+  try {
+    const home = await client.getSingle('home')
+    const cellCards = await client.getAllByType('cell_card')
+    const registerCardEvents = await client.getAllByType(
+      'register_card_event',
+      {
+        orderings: {
+          field: 'document.last_publication_date',
+          direction: 'desc',
+        },
+      },
+    )
+    return {
+      ...home,
+      context: {
+        registerCardEvents,
+        cellCards,
+      },
+    }
+  } catch (error: any) {
+    throw Error(error)
+  }
+}
