8c37d21a381e4c74c88f38dfd39405cbd57b756f
Guga Villar <grsv.21@gmail.com>
2023-08-05 09:34:10 -0300

    :sparkles: Feat: Validation date in EventCard and add NavigationButton on EventRegisterContainer

--- src/components/EventsComponents/EventGridContainer/EventCard.tsx
+++ src/components/EventsComponents/EventGridContainer/EventCard.tsx
@@ -1,7 +1,9 @@
 import { Card } from '@chakra-ui/react'
+import { isFuture, parseISO } from 'date-fns'
 
 import { CardBody } from '@/components/CardContent/CardBody'
 import { CardHeader } from '@/components/CardContent/CardHeader'
+import { IfComponent } from '@/components/IfComponent'
 
 import { EventCardType } from '../event'
 
@@ -10,25 +12,34 @@ type EventCardProps = {
 }
 
 export const EventCard = ({ items }: EventCardProps) => {
-  return items?.map((card, index) => (
-    <Card
-      key={index}
-      bg="serenade.50"
-      boxShadow="base"
-      borderRadius={0}
-      p={{
-        base: 4,
-        md: 6,
-        lg: '24px 26px 24px 40px',
-      }}
-      borderBottom="1rem solid transparent"
-      _hover={{
-        borderBottom: '1rem solid',
-        borderColor: '#FFD0A0',
-      }}
-    >
-      <CardHeader card={card} />
-      <CardBody card={card} gradientText="Próximo evento" />
-    </Card>
-  ))
+  return items?.map((card, index) => {
+    const isFutureDate = isFuture(parseISO(card?.final_date as string))
+
+    return (
+      <IfComponent
+        key={index}
+        condition={isFutureDate}
+        component={
+          <Card
+            bg="serenade.50"
+            boxShadow="base"
+            borderRadius={0}
+            p={{
+              base: 4,
+              md: 6,
+              lg: '24px 26px 24px 40px',
+            }}
+            borderBottom="1rem solid transparent"
+            _hover={{
+              borderBottom: '1rem solid',
+              borderColor: '#FFD0A0',
+            }}
+          >
+            <CardHeader card={card} />
+            <CardBody card={card} gradientText="Próximo evento" />
+          </Card>
+        }
+      />
+    )
+  })
 }
--- src/components/EventsComponents/EventRegisterContainer/index.tsx
+++ src/components/EventsComponents/EventRegisterContainer/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Image } from '@chakra-ui/react'
 
+import { NavigationButton } from '@/components/Buttons'
+
 import { EventRegisterCard } from './EventRegisterCard'
 import { RegisterCardType } from '../event'
 
@@ -7,26 +9,41 @@ type EventRegisterContainerProps = {
   items: Array<RegisterCardType>
 }
 
+const pathsToShow = ['/']
+
 export const EventRegisterContainer = ({
   items,
 }: EventRegisterContainerProps) => {
-  return items.map((item, index) => {
-    const isLastIndex = items?.length === index + 1
-    return (
-      <Flex
-        key={index}
-        direction={{ base: 'column-reverse', md: 'column-reverse', lg: 'row' }}
-        maxWidth="full"
-        mx="auto"
-        {...(!isLastIndex && { mt: 6 })}
-      >
-        <EventRegisterCard item={item} />
-        <Image
-          objectFit="cover"
-          src={item.image.url as string}
-          alt={item.image?.alt as string}
-        />
-      </Flex>
-    )
-  })
+  return (
+    <Flex direction="column" gap={8}>
+      {items.map((item, index) => {
+        const isLastIndex = items?.length === index + 1
+        return (
+          <Flex
+            key={index}
+            direction={{
+              base: 'column-reverse',
+              md: 'column-reverse',
+              lg: 'row',
+            }}
+            maxWidth="full"
+            mx="auto"
+            {...(!isLastIndex && { mt: 6 })}
+          >
+            <EventRegisterCard item={item} />
+            <Image
+              objectFit="cover"
+              src={item.image.url as string}
+              alt={item.image?.alt as string}
+            />
+          </Flex>
+        )
+      })}
+      <NavigationButton
+        pathsToShow={pathsToShow}
+        textButton="Veja todos os eventos"
+        pathRouter="/eventos"
+      />
+    </Flex>
+  )
 }
