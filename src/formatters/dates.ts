f518a8f61d910308b697760d88cbb2eeff7753e7
Guga Villar <grsv.21@gmail.com>
2023-08-05 18:42:14 -0300

    :sparkles: Feat: parseISO date-fns function

--- src/formatters/dates.ts
+++ src/formatters/dates.ts
@@ -1,21 +1,21 @@
-import { format, parse } from 'date-fns'
+import { format, parseISO } from 'date-fns'
 import * as locale from 'date-fns/locale'
 
 const ptBR = locale.ptBR
 
 export const dayOfMonth = (date: string) =>
-  format(parse(date, 'yyyy-MM-dd', new Date()), 'dd', {
+  format(parseISO(date), 'dd', {
     locale: ptBR,
   })
 
 export const monthAbbreviation = (date: string) =>
-  format(parse(date, 'yyyy-MM-dd', new Date()), 'MMMM', { locale: ptBR })
+  format(parseISO(date), 'MMMM', { locale: ptBR })
 
-export const dayOfWeekNameAndDayMonthYear = (date: Date) =>
-  format(date, 'cccc, dd/MM', { locale: ptBR })
+export const dayOfWeekNameAndDayMonthYear = (date: string) =>
+  format(parseISO(date), 'cccc, dd/MM', { locale: ptBR })
 
-export const hourAndMinutes = (date: Date) =>
-  format(date, 'HH:mm', { locale: ptBR })
+export const hourAndMinutes = (date: string) =>
+  format(parseISO(date), 'HH:mm', { locale: ptBR })
 
 export const dayOfWeekReplace = (day: string) => {
   if (!day) return ''
