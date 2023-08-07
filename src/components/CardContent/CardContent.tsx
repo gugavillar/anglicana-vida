714501c4afd7d384010cfbaf2612343b840fd172
Guga Villar <grsv.21@gmail.com>
2023-08-07 08:23:16 -0300

    :sparkles: Feat: Create SimpleCardsGrid component and CardContent component

---
+++ src/components/CardContent/CardContent.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+
+import { Card, CardProps } from '@chakra-ui/react'
+
+type CardContentProps = CardProps & {
+  children: ReactNode
+}
+
+export const CardContent = ({ children, ...props }: CardContentProps) => {
+  return (
+    <Card
+      borderRadius={0}
+      p={{
+        base: 4,
+        md: 6,
+        lg: '24px 26px 24px 40px',
+      }}
+      borderBottom="1rem solid transparent"
+      _hover={{
+        borderBottom: '1rem solid',
+        borderColor: '#FFD0A0',
+      }}
+      {...props}
+    >
+      {children}
+    </Card>
+  )
+}
