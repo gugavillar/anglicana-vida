5006fdae25e9d0e97a34632ed483e7ca5106bea4
Guga Villar <grsv.21@gmail.com>
2024-04-14 15:52:30 -0300

    :sparkles: Feat: add whats app link

--- src/components/WhatsappButton/index.tsx
+++ src/components/WhatsappButton/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { memo } from 'react'
 
 import { Button } from '@chakra-ui/react'
@@ -5,24 +6,30 @@ import { Button } from '@chakra-ui/react'
 import { WhatsappLogo } from 'phosphor-react'
 
 export const WhatsappButton = memo(() => {
+  const message = encodeURI(
+    'Olá, estou interessado em participar das atividades da igreja. Como posso me envolver?',
+  )
+
   return (
-    <Button
-      position="fixed"
-      borderRadius="full"
-      bottom={6}
-      right={6}
-      zIndex={9999}
-      width="fit-content"
-      height="fit-content"
-      p={2}
-      iconSpacing={0}
-      bg="whatsapp.500"
-      _hover={{
-        bg: 'whatsapp.700',
-        transition: 'background-color 300ms linear',
-      }}
-      leftIcon={<WhatsappLogo size="32" />}
-    />
+    <Link href={`https://wa.me/5581995208327/?text=${message}`} target="_blank">
+      <Button
+        position="fixed"
+        borderRadius="full"
+        bottom={6}
+        right={6}
+        zIndex={9999}
+        width="fit-content"
+        height="fit-content"
+        p={2}
+        iconSpacing={0}
+        bg="whatsapp.500"
+        _hover={{
+          bg: 'whatsapp.700',
+          transition: 'background-color 300ms linear',
+        }}
+        leftIcon={<WhatsappLogo size="32" />}
+      />
+    </Link>
   )
 })
 
