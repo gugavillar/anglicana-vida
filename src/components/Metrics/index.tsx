3b2bf2a8429916dee443981d05ffaf0c117859e1
Guga Villar <grsv.21@gmail.com>
2024-07-24 07:38:58 -0300

    :sparkles: Clarity adjustments

--- src/components/Metrics/index.tsx
+++ src/components/Metrics/index.tsx
@@ -1,18 +1,14 @@
 import Script from 'next/script'
 export const MicrosoftClarity = () => {
   return (
-    <Script
-      id="microsoft-clarity-init"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-                (function(c,l,a,r,i,t,y) {
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "nc3dhidq1p");
-                `,
-      }}
-    />
+    <Script id="microsoft-clarity-init" strategy="afterInteractive">
+      {`
+        (function(c,l,a,r,i,t,y) {
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "nc3dhidq1p");
+      `}
+    </Script>
   )
 }
